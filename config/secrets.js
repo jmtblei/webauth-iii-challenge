0ec4229429c2e5bf435db292fdd01b0581abb974
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-08 12:36:54 -0700

    migrations, seeds,

---
+++ config/secrets.js
@@ -0,0 +1,7 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || 'add this variable to your .env file',
+  
+    secretword: "ultratopsecret",
+    howsecret: "pinkypromise swearonyourlife super duper secret",
+};
+  
